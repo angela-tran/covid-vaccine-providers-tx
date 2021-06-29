0f0f03ff2633e933af5d8b3bc0facaee581fd819
Angela Tran <angela.t.tran0+git@gmail.com>
2021-06-29 08:14:12 -0500

    Add providers.py script to create a CSV of data from the Texas COVID-19 Vaccine Providers Dashboard
    This is the script as-is from when I wrote it on Jan 06, 2021 to help with an effort to crowdsource vaccine availability information.

---
+++ providers.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import csv
+
+def main():
+    offset = 0
+    page_size = 200
+
+    provider_count_url = "https://services5.arcgis.com/ACaLB9ifngzawspq/ArcGIS/rest/services/VaccineProviderLocations/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=true&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=0&resultRecordCount=200&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token="
+
+    provider_count = int(json.loads(requests.get(provider_count_url).text)["count"])
+
+    field_names = ["OBJECTID", "ProviderNa", "Address", "City", "State", "Zip", "County", "Phone", "Website", "PfizerTota", "ModernaTot", "OrgGroup", "Allocation", "COVID_ID", "Provider_P"]
+
+    with open('providers.csv', mode='w') as output_file:
+        providers_writer = csv.writer(output_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        providers_writer.writerow(field_names)
+
+        while (offset < provider_count): 
+            page = json.loads(requests.get(provider_info_url(offset, page_size)).text)["features"]
+            offset += page_size
+
+            for p in page:
+                provider_attributes = p["attributes"]
+                row = []
+                for f in field_names:
+                    row.append(provider_attributes[f])
+                providers_writer.writerow(row)
+
+def provider_info_url(offset, page_size):
+    return f"https://services5.arcgis.com/ACaLB9ifngzawspq/ArcGIS/rest/services/VaccineProviderLocations/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset={offset}&resultRecordCount={page_size}&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token="
+
+if __name__ == '__main__':
+    main()
